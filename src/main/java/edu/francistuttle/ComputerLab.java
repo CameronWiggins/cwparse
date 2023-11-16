afa291eb7309a598a3e094ee64b8a329b6c3180d
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-11-16 08:33:01 -0600

    classes complete

---
+++ src/main/java/edu/francistuttle/ComputerLab.java
@@ -0,0 +1,12 @@
+package edu.francistuttle;
+import java.util.ArrayList;
+
+public class ComputerLab 
+{
+    public ArrayList<Computer> parse(String pathname)
+    {
+        ArrayList<Computer> computers = new ArrayList<Computer>();
+        
+        return computers;
+    }
+}
